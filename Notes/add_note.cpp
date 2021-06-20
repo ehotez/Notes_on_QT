39418d5dd397f5bf2fb99f4b7970048a27a8e32d
ehotez <85438019+ehotez@users.noreply.github.com>
2021-06-20 15:15:15 +0300

    Delete Notes directory

--- Notes/add_note.cpp
+++
@@ -1,8 +0,0 @@
-#include "add_note.h"
-
-add_note::add_note(QWidget *parent) : QWidget(parent)
-{
-    box = new QVBoxLayout(this);
-    text = new QPlainTextEdit(this);
-    box->addWidget(text);
-}
